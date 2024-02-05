c5405c0b50dc1ec7b287a3d6ed0a36f5e8eb4f01
garrethil <garrethildebrandt@gmail.com>
2024-02-04 19:57:12 -0500

    write basic generateSVG function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const inquirer = require("inquirer");
-const Triangle = require("./lib/shapes.js")
+const Triangle = require("./lib/shapes.js");
+const Square = require("./lib/shapes.js");
+const Circle = require("./lib/shapes.js");
 const fs = require("fs");
 
 
+const generateSVG = ({
+    text, textColour, shapeColour,
+}) => 
+`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
+<polygon points="${this.polyPoints}" fill="${shapeColour}"/>
 
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>
+
+</svg>`;
 
 inquirer.prompt([
     {
